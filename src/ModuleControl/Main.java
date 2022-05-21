6276cc1ee21f41be0dd3caa53b7cbfa950d5a027
Mykyta Balandin <92630318+nikitaProger04@users.noreply.github.com>
2022-05-21 08:57:08 +0200

    Add main and people classes

---
+++ src/ModuleControl/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    private String name;
+
+    private boolean canGoToWar;
+
+    private String rank;
+    public static void main(String[] args) {
+        People people = new People();
+
+    }
+
+}
---
+++ src/ModuleControl/People.java
@@ -0,0 +1,2 @@
+package ModuleControl;public class People {
+}
