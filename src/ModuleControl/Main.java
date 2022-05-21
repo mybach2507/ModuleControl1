cb585e109ae648b71616fb8ec2856ad5057d4c0e
Mykyta Balandin <92630318+nikitaProger04@users.noreply.github.com>
2022-05-21 09:05:09 +0200

    Add dependencies searcher

--- src/ModuleControl/Main.java
+++ src/ModuleControl/Main.java
@@ -1,12 +1,37 @@
+package ModuleControl;
 
 public class Main {
-    private String name;
 
-    private boolean canGoToWar;
 
-    private String rank;
+
+
     public static void main(String[] args) {
-        People people = new People();
+        People people1 = new People();
+        People people2 = new People();
+        People people3 = new People();
+        People people4 = new People();
+        People people5 = new People();
+        People people6 = new People();
+        People people7 = new People();
+        People people8 = new People();
+        People people9 = new People();
+        People people10 = new People();
+
+
+        people1.setRank("soldier");
+        people2.setRank("soldier");
+        people3.setRank("soldier");
+        people4.setRank("soldier");
+        people5.setRank("soldier");
+        people6.setRank("soldier");
+        people7.setRank("captain");
+        people8.setRank("captain");
+        people9.setRank("general");
+        people10.setRank("general");
+
+
+
+
 
     }
 
--- src/ModuleControl/People.java
+++ src/ModuleControl/People.java
@@ -1,2 +1,103 @@
-package ModuleControl;public class People {
+package ModuleControl;
+
+public class People {
+    private String name;
+
+    private boolean canGoToWar;
+
+    private String rank;
+
+    private String soldier;
+
+    private String captain;
+
+    private String general;
+
+    private boolean ableToSay;
+    private boolean hasDependencies;
+    private int size;
+    private String[] array = new String[size];
+
+    public People()
+    {
+        this.rank=soldier;
+        this.canGoToWar=true;
+    }
+    public String getRank(String rank)
+    {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+    public void setSize(int size)
+    {
+        this.size=size;
+    }
+    public void addPeople(String rank)
+    {
+        int counter=0;
+        String[] array = new String[size];
+
+        if(array[counter] == null){
+            counter++;
+            for (int i = 1;i<size; i++) {
+                if(array[i]==null)
+                {
+                    array[i] = name;
+                }
+                if(counter==size){
+                    break;
+                }
+            }
+        }
+    }
+    public void searchPeople(String rank)
+    {
+
+    }
+    public void printDependencies(String rank)
+    {
+        int soldierCounter = 0;
+        for(int i = 0; i<size; i++)
+        {
+            if(this.rank==soldier)
+            {
+                soldierCounter++;
+            }
+        }
+        int captainCounter = 0;
+        for(int i = 0; i<size; i++)
+        {
+            if(this.rank==captain)
+            {
+                int dependencies=0;
+                for(int y=0;y<size;y++)
+                {
+                    if(array[y]==soldier)
+                    {
+                        dependencies++;
+                        System.out.println("captain has "+dependencies+" dependencies");
+                    }
+                }
+                captainCounter++;
+            }
+        }
+        int generalCounter = 0;
+        for(int i = 0; i<size; i++)
+        {
+            if(this.rank==general)
+            {
+                captainCounter++;
+            }
+        }
+
+    }
+    public interface ableToWar{
+
+    }
+
+
 }
+
